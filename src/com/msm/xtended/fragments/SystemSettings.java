07deef53ac8a25d9a7d4cc74eb4bc5f3f66555c8
Alex Cruz <mazdarider23@gmail.com>
2017-06-24 16:13:35 +0200

    Xtensions: Add SELinux Switch
    Squashed commits:
    
    commit ce05f7dc7ad333b1234fee001e54ebd3e695634e
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Thu Dec 22 18:05:55 2016 +0100
    
        Selinux Switch
    
        Change-Id: Idd38f844f94bdc81e2b690682c73e468156ea10b
    
    commit 6b29cf14f8c25f8ee476d86225da3faa7c632a21
    Author: LorDClockaN <lordclockan@gmail.com>
    Date:   Thu Dec 22 18:11:05 2016 +0100
    
        Selinux Switch: Show toast only for permissive on boot
    
        Change-Id: I4e0589e09904332cf0dead5fe2bc9f416caf812e
    
    commit 217411c19ac0c0ac0bfd47b15d00f3d33406fa57
    Author: Meninblack007 <sanyam.53jain@gmail.com>
    Date:   Thu Dec 22 18:57:39 2016 +0100
    
        Settings: Fix selinux switch (#601)
    
        * Cleanup whitespaces
        * Use SharedPreference to save values
    
        Change-Id: I8dd13c3d2f6a16e4633933b5d750c9db412aa8e5
        Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>
    
    Author: SpiritCroc <spiritcroc@gmail.com>
    
        Use our SuShell utils, handle su denied, use boolean in
        shared pref, enhance our root extras fragment for
        initialization, ...
    
    Change-Id: I2469c45a794f960102534efc97f2e0c26e812987
    
    commit 75b568bfb11c70f6d386d7d27cc34aff9d550237
    Author: SpiritCroc <spiritcroc@gmail.com>
    
    @idoybh (DerpFest) edits: Adapt, organize, improve some code
    
    Co-authored-by: SpiritCroc <spiritcroc@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: I09c64af474517f3642bcd336b538027c84b66dc8

--- src/com/msm/xtended/fragments/SystemSettings.java
+++ src/com/msm/xtended/fragments/SystemSettings.java
@@ -2,6 +2,8 @@ package com.msm.xtended.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -11,6 +13,7 @@ import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.SELinux;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import androidx.preference.ListPreference;
@@ -23,6 +26,10 @@ import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 import com.android.settings.R;
+import android.util.Log;
+
+import com.msm.xtended.utils.SuTask;
+import com.msm.xtended.utils.SuShell;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -31,6 +38,15 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class SystemSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private static final String TAG = "SystemSettings";
+
+    private static final String SELINUX_CATEGORY = "selinux";
+    private static final String PREF_SELINUX_MODE = "selinux_mode";
+    private static final String PREF_SELINUX_PERSISTENCE = "selinux_persistence";
+
+    private SwitchPreference mSelinuxMode;
+    private SwitchPreference mSelinuxPersistence;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -44,16 +60,75 @@ public class SystemSettings extends SettingsPreferenceFragment implements
         if (!enableSmartPixels){
             getPreferenceScreen().removePreference(SmartPixels);
         }
+      // SELinux
+      Preference selinuxCategory = findPreference(SELINUX_CATEGORY);
+      mSelinuxMode = (SwitchPreference) findPreference(PREF_SELINUX_MODE);
+      mSelinuxMode.setChecked(SELinux.isSELinuxEnforced());
+      mSelinuxMode.setOnPreferenceChangeListener(this);
+
+      mSelinuxPersistence =
+          (SwitchPreference) findPreference(PREF_SELINUX_PERSISTENCE);
+      mSelinuxPersistence.setOnPreferenceChangeListener(this);
+      mSelinuxPersistence.setChecked(getContext()
+          .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE)
+          .contains(PREF_SELINUX_MODE));
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+      if (preference == mSelinuxMode) {
+        boolean enabled = (Boolean) objValue;
+        new SwitchSelinuxTask(getActivity()).execute(enabled);
+        setSelinuxEnabled(enabled, mSelinuxPersistence.isChecked());
+        return true;
+      } else if (preference == mSelinuxPersistence) {
+        setSelinuxEnabled(mSelinuxMode.isChecked(), (Boolean) objValue);
+        return true;
+      }
         return false;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.XTENSIONS;
+    }
+
+        private void setSelinuxEnabled(boolean status, boolean persistent) {
+      SharedPreferences.Editor editor = getContext()
+          .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE).edit();
+      if (persistent) {
+        editor.putBoolean(PREF_SELINUX_MODE, status);
+      } else {
+        editor.remove(PREF_SELINUX_MODE);
+      }
+      editor.apply();
+      mSelinuxMode.setChecked(status);
+    }
+
+    private class SwitchSelinuxTask extends SuTask<Boolean> {
+      public SwitchSelinuxTask(Context context) {
+        super(context);
+      }
+      @Override
+      protected void sudoInBackground(Boolean... params) throws SuShell.SuDeniedException {
+        if (params.length != 1) {
+          Log.e(TAG, "SwitchSelinuxTask: invalid params count");
+          return;
+        }
+        if (params[0]) {
+          SuShell.runWithSuCheck("setenforce 1");
+        } else {
+          SuShell.runWithSuCheck("setenforce 0");
+        }
+      }
+
+      @Override
+      protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+          // Did not work, so restore actual value
+          setSelinuxEnabled(SELinux.isSELinuxEnforced(), mSelinuxPersistence.isChecked());
+        }
+      }
     }
 }
