b8dd969b53231ae9359b08f416a54bf682534fcf
SuperDroidBond <superdroidbond@yahoo.com>
2018-04-25 18:38:40 +0530

    Default Value to Expanded Notifications Also remove Preference for Safe Volume

--- src/com/xtended/xtensions/fragments/VolumeRockerSettings.java
+++ src/com/xtended/xtensions/fragments/VolumeRockerSettings.java
@@ -11,6 +11,8 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
@@ -38,22 +40,18 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
         mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
         if (mVolumeKeyCursorControl != null) {
             mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
+
             int volumeRockerCursorControl = Settings.System.getInt(getContentResolver(),
                     Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
             mVolumeKeyCursorControl.setValue(Integer.toString(volumeRockerCursorControl));
             mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
         }
-
-        mSafeHeadsetVolume = (SwitchPreference) findPreference(KEY_SAFE_HEADSET_VOLUME);
-        mSafeHeadsetVolume.setChecked(Settings.System.getInt(resolver,
-				Settings.System.SAFE_HEADSET_VOLUME, 1) != 0);
-        mSafeHeadsetVolume.setOnPreferenceChangeListener(this);
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object value) {
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
         if (preference == mVolumeKeyCursorControl) {
-            String volumeKeyCursorControl = (String) value;
+            String volumeKeyCursorControl = (String) objValue;
             int volumeKeyCursorControlValue = Integer.parseInt(volumeKeyCursorControl);
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.VOLUME_KEY_CURSOR_CONTROL, volumeKeyCursorControlValue);
@@ -62,13 +60,6 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
             mVolumeKeyCursorControl
                     .setSummary(mVolumeKeyCursorControl.getEntries()[volumeKeyCursorControlIndex]);
             return true;
-        } else if (KEY_SAFE_HEADSET_VOLUME.equals(key)) {
-            if ((Boolean) newValue) {
-                    Settings.System.putInt(getActivity().getContentResolver(),
-                            Settings.System.SAFE_HEADSET_VOLUME, 1);
-            } else {
-                    showDialogInner(DLG_SAFE_HEADSET_VOLUME);
-            }
         }
         return false;
     }
@@ -78,61 +69,4 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
         return MetricsProto.MetricsEvent.XTENDED;
     }
 
-
-    private void showDialogInner(int id) {
-        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
-        newFragment.setTargetFragment(this, 0);
-        newFragment.show(getFragmentManager(), "dialog "  id);
-    }
-
-    public static class MyAlertDialogFragment extends DialogFragment {
-
-        public static MyAlertDialogFragment newInstance(int id) {
-            MyAlertDialogFragment frag = new MyAlertDialogFragment();
-            Bundle args = new Bundle();
-            args.putInt("id", id);
-            frag.setArguments(args);
-            return frag;
-        }
-
-        VolumeRockerSettings getOwner() {
-            return (VolumeRockerSettings) getTargetFragment();
-        }
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            int id = getArguments().getInt("id");
-                switch (id) {
-                   case DLG_SAFE_HEADSET_VOLUME:
-                       return new AlertDialog.Builder(getActivity())
-                           .setTitle(R.string.attention)
-                           .setMessage(R.string.safe_headset_volume_warning_dialog_text)
-                           .setPositiveButton(R.string.ok,
-                               new DialogInterface.OnClickListener() {
-                                   public void onClick(DialogInterface dialog, int which) {
-                                       Settings.System.putInt(getOwner().getActivity().getContentResolver(),
-                                       Settings.System.SAFE_HEADSET_VOLUME, 0);
-                                   }
-                               })
-                           .setNegativeButton(R.string.cancel,
-                               new DialogInterface.OnClickListener() {
-                                   public void onClick(DialogInterface dialog, int which) {
-                                       dialog.cancel();
-                                   }
-                               })
-                           .create();
-                }
-                   throw new IllegalArgumentException("unknown id "  id);
-        }
-
-        @Override
-        public void onCancel(DialogInterface dialog) {
-            int id = getArguments().getInt("id");
-                switch (id) {
-                   case DLG_SAFE_HEADSET_VOLUME:
-                       getOwner().mSafeHeadsetVolume.setChecked(true);
-                       break;
-                   }
-        }
-   }
 }
