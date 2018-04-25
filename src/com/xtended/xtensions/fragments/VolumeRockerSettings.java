228cdc3ca3d655ee684985488798e20fbb54d79f
SuperDroidBond <superdroidbond@yahoo.com>
2018-04-25 18:37:40 +0530

    Welcome Back Skip Music & Safe Headset Volume Option

--- src/com/xtended/xtensions/fragments/VolumeRockerSettings.java
+++ src/com/xtended/xtensions/fragments/VolumeRockerSettings.java
@@ -2,6 +2,12 @@ package com.xtended.xtensions.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -16,8 +22,11 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+    private static final int DLG_SAFE_HEADSET_VOLUME = 0;
+    private static final String KEY_SAFE_HEADSET_VOLUME = "safe_headset_volume";
 
     private ListPreference mVolumeKeyCursorControl;
+    private SwitchPreference mSafeHeadsetVolume;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -34,6 +43,11 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
             mVolumeKeyCursorControl.setValue(Integer.toString(volumeRockerCursorControl));
             mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
         }
+
+        mSafeHeadsetVolume = (SwitchPreference) findPreference(KEY_SAFE_HEADSET_VOLUME);
+        mSafeHeadsetVolume.setChecked(Settings.System.getInt(resolver,
+				Settings.System.SAFE_HEADSET_VOLUME, 1) != 0);
+        mSafeHeadsetVolume.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -48,6 +62,13 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
             mVolumeKeyCursorControl
                     .setSummary(mVolumeKeyCursorControl.getEntries()[volumeKeyCursorControlIndex]);
             return true;
+        } else if (KEY_SAFE_HEADSET_VOLUME.equals(key)) {
+            if ((Boolean) newValue) {
+                    Settings.System.putInt(getActivity().getContentResolver(),
+                            Settings.System.SAFE_HEADSET_VOLUME, 1);
+            } else {
+                    showDialogInner(DLG_SAFE_HEADSET_VOLUME);
+            }
         }
         return false;
     }
@@ -56,4 +77,62 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.XTENDED;
     }
+
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog "  id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        VolumeRockerSettings getOwner() {
+            return (VolumeRockerSettings) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+                switch (id) {
+                   case DLG_SAFE_HEADSET_VOLUME:
+                       return new AlertDialog.Builder(getActivity())
+                           .setTitle(R.string.attention)
+                           .setMessage(R.string.safe_headset_volume_warning_dialog_text)
+                           .setPositiveButton(R.string.ok,
+                               new DialogInterface.OnClickListener() {
+                                   public void onClick(DialogInterface dialog, int which) {
+                                       Settings.System.putInt(getOwner().getActivity().getContentResolver(),
+                                       Settings.System.SAFE_HEADSET_VOLUME, 0);
+                                   }
+                               })
+                           .setNegativeButton(R.string.cancel,
+                               new DialogInterface.OnClickListener() {
+                                   public void onClick(DialogInterface dialog, int which) {
+                                       dialog.cancel();
+                                   }
+                               })
+                           .create();
+                }
+                   throw new IllegalArgumentException("unknown id "  id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            int id = getArguments().getInt("id");
+                switch (id) {
+                   case DLG_SAFE_HEADSET_VOLUME:
+                       getOwner().mSafeHeadsetVolume.setChecked(true);
+                       break;
+                   }
+        }
+   }
 }
