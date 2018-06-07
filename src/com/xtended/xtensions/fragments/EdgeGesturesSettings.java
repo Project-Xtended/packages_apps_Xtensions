Havoc-OS/android_packages_apps_HavocSettings

Add edge gestures (2/2)

src/com/havoc/settings/fragments/EdgeGesturesSettings.java
@@ -1,0 +1,96 @@
+package com.havoc.settings.fragments; 
+ 
+import android.app.ActionBar; 
+import android.os.Bundle; 
+import android.os.UserHandle; 
+import android.provider.Settings; 
+import android.support.v14.preference.SwitchPreference; 
+import android.support.v7.preference.Preference; 
+ 
+import com.android.internal.logging.nano.MetricsProto; 
+import com.android.internal.utils.du.DUActionUtils; 
+import com.android.settings.R; 
+import com.android.settings.SettingsPreferenceFragment; 
+import com.havoc.settings.preferences.SecureSettingSeekBarPreference; 
+ 
+public class EdgeGesturesSettings extends SettingsPreferenceFragment implements 
+        Preference.OnPreferenceChangeListener { 
+ 
+    public static final String EDGE_GESTURES_ENABLED = "edge_gestures_enabled"; 
+    public static final String EDGE_GESTURES_SCREEN_PERCENT = "edge_gestures_back_screen_percent"; 
+ 
+    private String previousTitle; 
+ 
+    private SwitchPreference enabledPreference; 
+    private SecureSettingSeekBarPreference screenPercentPreference; 
+ 
+    @Override 
+    public void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState); 
+ 
+        addPreferencesFromResource(R.xml.edge_gestures); 
+ 
+        enabledPreference = (SwitchPreference) findPreference(EDGE_GESTURES_ENABLED); 
+        /*enabledPreference.setChecked((Settings.System.getInt(getContentResolver(), 
+                Settings.Secure.EDGE_GESTURES_ENABLED, 0) == 1));*/ 
+        enabledPreference.setOnPreferenceChangeListener(this); 
+ 
+        screenPercentPreference = (SecureSettingSeekBarPreference) findPreference(EDGE_GESTURES_SCREEN_PERCENT); 
+        int percent = Settings.Secure.getIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_BACK_SCREEN_PERCENT, 60, UserHandle.USER_CURRENT); 
+        screenPercentPreference.setValue(percent); 
+        screenPercentPreference.setIntervalValue(5); 
+    } 
+ 
+    @Override 
+    public void onStart() { 
+        super.onStart(); 
+ 
+        ActionBar actionBar = getActivity().getActionBar(); 
+        previousTitle = actionBar.getTitle().toString(); 
+        actionBar.setTitle(R.string.edge_gestures_title); 
+    } 
+ 
+    @Override 
+    public void onStop() { 
+        super.onStop(); 
+ 
+        ActionBar actionBar = getActivity().getActionBar(); 
+        actionBar.setTitle(previousTitle); 
+    } 
+ 
+    @Override 
+    public int getMetricsCategory() { 
+        return MetricsProto.MetricsEvent.HAVOC_SETTINGS; 
+    } 
+ 
+    @Override 
+    public boolean onPreferenceChange(Preference preference, Object newValue) { 
+        if (preference == enabledPreference) { 
+            int enabled = ((boolean) newValue) ? 1 : 0; 
+            //Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_ENABLED, enabled, UserHandle.USER_CURRENT); 
+ 
+            if (enabled == 1) { 
+                Settings.Secure.putInt(getContentResolver(), 
+                        Settings.Secure.NAVIGATION_BAR_VISIBLE, 
+                        0); 
+            } else { 
+                if (DUActionUtils.hasNavbarByDefault(getPrefContext())) { 
+                    Settings.Secure.putInt(getContentResolver(), 
+                            Settings.Secure.NAVIGATION_BAR_VISIBLE, 
+                            1); 
+                } 
+            } 
+            return true; 
+        } /*else if (preference == hapticFeedbackDurationPreference) { 
+            int hapticFeedbackValue = Integer.valueOf((String) newValue); 
+            Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_FEEDBACK_DURATION, hapticFeedbackValue, UserHandle.USER_CURRENT); 
+            return true; 
+        } else if (preference == longPressDurationPreference) { 
+            int longPressValue = Integer.valueOf((String) newValue); 
+            Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_LONG_PRESS_DURATION, longPressValue, UserHandle.USER_CURRENT); 
+            return true; 
+        }*/ 
+ 
+        return false; 
+    } 
+} 

src/com/havoc/settings/preferences/SecureSettingIntListPreference.java
@@ -1,0 +1,47 @@
+/* 
+ * Copyright (C) 2017 AOSPEXTENDED PROJECT 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */ 
+ 
+package com.havoc.settings.preferences; 
+ 
+import android.content.Context; 
+import android.util.AttributeSet; 
+ 
+public class SecureSettingIntListPreference extends SecureSettingListPreference { 
+ 
+    public SecureSettingIntListPreference(Context context, AttributeSet attrs, int defStyle) { 
+        super(context, attrs, defStyle); 
+    } 
+ 
+    public SecureSettingIntListPreference(Context context, AttributeSet attrs) { 
+        super(context, attrs); 
+    } 
+ 
+    public SecureSettingIntListPreference(Context context) { 
+        super(context); 
+    } 
+ 
+    @Override 
+    protected boolean persistString(String value) { 
+        return persistInt(Integer.parseInt(value)); 
+    } 
+ 
+    @Override 
+    protected String getPersistedString(String defaultReturnValue) { 
+        return String.valueOf(getPersistedInt(Integer.parseInt(defaultReturnValue))); 
+    } 
+ 
+} 
+ 

src/com/havoc/settings/preferences/SecureSettingListPreference.java
@@ -1,0 +1,77 @@
+/* 
+ * Copyright (C) 2017 AICP 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */ 
+ 
+package com.havoc.settings.preferences; 
+ 
+import android.content.Context; 
+import android.provider.Settings; 
+import android.support.v7.preference.ListPreference; 
+import android.text.TextUtils; 
+import android.util.AttributeSet; 
+ 
+public class SecureSettingListPreference extends ListPreference { 
+ 
+    private boolean mAutoSummary = false; 
+ 
+    public SecureSettingListPreference(Context context, AttributeSet attrs, int defStyle) { 
+        super(context, attrs, defStyle); 
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver())); 
+    } 
+ 
+    public SecureSettingListPreference(Context context, AttributeSet attrs) { 
+        super(context, attrs); 
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver())); 
+    } 
+ 
+    public SecureSettingListPreference(Context context) { 
+        super(context); 
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver())); 
+    } 
+ 
+    @Override 
+    public void setValue(String value) { 
+        super.setValue(value); 
+        if (mAutoSummary || TextUtils.isEmpty(getSummary())) { 
+            setSummary(getEntry(), true); 
+        } 
+    } 
+ 
+    @Override 
+    public void setSummary(CharSequence summary) { 
+        setSummary(summary, false); 
+    } 
+ 
+    private void setSummary(CharSequence summary, boolean autoSummary) { 
+        mAutoSummary = autoSummary; 
+        super.setSummary(summary); 
+    } 
+ 
+    @Override 
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) { 
+        // This is what default ListPreference implementation is doing without respecting 
+        // real default value: 
+        //setValue(restoreValue ? getPersistedString(mValue) : (String) defaultValue); 
+        // Instead, we better do 
+        setValue(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue); 
+    } 
+ 
+    @Override 
+    protected boolean isPersisted() { 
+        // Using getString instead of getInt so we can simply check for null 
+        // instead of catching an exception. (All values are stored as strings.) 
+        return Settings.Secure.getString(getContext().getContentResolver(), getKey()) != null; 
+    } 
+} 

src/com/havoc/settings/preferences/SecureSettingSeekBarPreference.java
@@ -1,0 +1,49 @@
+/* 
+ * Copyright (C) 2017 AOSPEXTENDED PROJECT 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */ 
+ 
+package com.havoc.settings.preferences; 
+ 
+import android.content.Context; 
+import android.content.res.TypedArray; 
+import android.util.AttributeSet; 
+ 
+public class SecureSettingSeekBarPreference extends CustomSeekBarPreference { 
+ 
+    public SecureSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) { 
+        super(context, attrs, defStyleAttr, defStyleRes); 
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver())); 
+    } 
+ 
+    public SecureSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) { 
+        super(context, attrs, defStyle); 
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver())); 
+    } 
+ 
+    public SecureSettingSeekBarPreference(Context context, AttributeSet attrs) { 
+        super(context, attrs); 
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver())); 
+    } 
+ 
+    public SecureSettingSeekBarPreference(Context context) { 
+        super(context, null); 
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver())); 
+    } 
+ 
+    @Override 
+    protected Object onGetDefaultValue(TypedArray ta, int index) { 
+        return super.onGetDefaultValue(ta, index); 
+    } 
+} 

Project-Xtended/packages_apps_Xtensions

src/com/xtended/xtensions/fragments/EdgeGesturesSettings.java
@@ -1,0 +1,97 @@
+package com.xtended.xtensions.fragments;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.utils.du.DUActionUtils;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.xtended.xtensions.preferences.SecureSettingSeekBarPreference;
+
+public class EdgeGesturesSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    public static final String EDGE_GESTURES_ENABLED = "edge_gestures_enabled";
+    public static final String EDGE_GESTURES_SCREEN_PERCENT = "edge_gestures_back_screen_percent";
+
+    private String previousTitle;
+
+    private SwitchPreference enabledPreference;
+    private SecureSettingSeekBarPreference screenPercentPreference;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.edge_gestures);
+
+        enabledPreference = (SwitchPreference) findPreference(EDGE_GESTURES_ENABLED);
+        /*enabledPreference.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.Secure.EDGE_GESTURES_ENABLED, 0) == 1));*/
+        enabledPreference.setOnPreferenceChangeListener(this);
+
+        screenPercentPreference = (SecureSettingSeekBarPreference) findPreference(EDGE_GESTURES_SCREEN_PERCENT);
+        int percent = Settings.Secure.getIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_BACK_SCREEN_PERCENT, 60, UserHandle.USER_CURRENT);
+        screenPercentPreference.setValue(percent);
+        screenPercentPreference.setIntervalValue(5);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        ActionBar actionBar = getActivity().getActionBar();
+        previousTitle = actionBar.getTitle().toString();
+        actionBar.setTitle(R.string.edge_gestures_title);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setTitle(previousTitle);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.XTENDED;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == enabledPreference) {
+            int enabled = ((boolean) newValue) ? 1 : 0;
+            //Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_ENABLED, enabled, UserHandle.USER_CURRENT);
+
+            if (enabled == 1) {
+                Settings.Secure.putInt(getContentResolver(),
+                        Settings.Secure.NAVIGATION_BAR_VISIBLE,
+                        0);
+            } else {
+                if (DUActionUtils.hasNavbarByDefault(getPrefContext())) {
+                    Settings.Secure.putInt(getContentResolver(),
+                            Settings.Secure.NAVIGATION_BAR_VISIBLE,
+                            1);
+                }
+            }
+            return true;
+        } /*else if (preference == hapticFeedbackDurationPreference) {
+            int hapticFeedbackValue = Integer.valueOf((String) newValue);
+            Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_FEEDBACK_DURATION, hapticFeedbackValue, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == longPressDurationPreference) {
+            int longPressValue = Integer.valueOf((String) newValue);
+            Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_LONG_PRESS_DURATION, longPressValue, UserHandle.USER_CURRENT);
+            return true;
+        }*/
+
+        return false;
+    }
+}
+

src/com/xtended/xtensions/preferences/SecureSettingIntListPreference.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright (C) 2017 AOSPEXTENDED PROJECT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xtended.xtensions.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SecureSettingIntListPreference extends SecureSettingListPreference {
+
+    public SecureSettingIntListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SecureSettingIntListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SecureSettingIntListPreference(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected boolean persistString(String value) {
+        return persistInt(Integer.parseInt(value));
+    }
+
+    @Override
+    protected String getPersistedString(String defaultReturnValue) {
+        return String.valueOf(getPersistedInt(Integer.parseInt(defaultReturnValue)));
+    }
+
+}
+

src/com/xtended/xtensions/preferences/SecureSettingListPreference.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xtended.xtensions.preferences;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+public class SecureSettingListPreference extends ListPreference {
+
+    private boolean mAutoSummary = false;
+
+    public SecureSettingListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingListPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    @Override
+    public void setValue(String value) {
+        super.setValue(value);
+        if (mAutoSummary || TextUtils.isEmpty(getSummary())) {
+            setSummary(getEntry(), true);
+        }
+    }
+
+    @Override
+    public void setSummary(CharSequence summary) {
+        setSummary(summary, false);
+    }
+
+    private void setSummary(CharSequence summary, boolean autoSummary) {
+        mAutoSummary = autoSummary;
+        super.setSummary(summary);
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        // This is what default ListPreference implementation is doing without respecting
+        // real default value:
+        //setValue(restoreValue ? getPersistedString(mValue) : (String) defaultValue);
+        // Instead, we better do
+        setValue(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue);
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        // Using getString instead of getInt so we can simply check for null
+        // instead of catching an exception. (All values are stored as strings.)
+        return Settings.Secure.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+}
+

src/com/xtended/xtensions/preferences/SecureSettingSeekBarPreference.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2017 AOSPEXTENDED PROJECT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xtended.xtensions.preferences;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+
+public class SecureSettingSeekBarPreference extends CustomSeekBarPreference {
+
+    public SecureSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingSeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingSeekBarPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray ta, int index) {
+        return super.onGetDefaultValue(ta, index);
+    }
+}
+