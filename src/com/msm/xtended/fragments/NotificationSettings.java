2d5c9d1c2ad94087260c5adc32ad8a0050ce816a
beanstown106 <nbenis106@gmail.com>
2018-09-06 22:52:55 +0530

    incall vibration options [3/3] *allow setting vibration when call is connected *allow setting vibration when call is disconnected *allow setting vibration for call waiting *this works with google and aosp dialer :) SuperDroidBond-: Make it default on

--- src/com/msm/xtended/fragments/NotificationSettings.java
+++ src/com/msm/xtended/fragments/NotificationSettings.java
@@ -1,6 +1,9 @@
 package com.msm.xtended.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import com.msm.xtended.preferences.XUtils;
 
 import android.os.Bundle;
 import com.android.settings.R;
@@ -9,11 +12,19 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class NotificationSettings extends SettingsPreferenceFragment {
 
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.x_settings_notifications);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!XUtils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(incallVibCategory);
+        }
     }
 
     @Override
