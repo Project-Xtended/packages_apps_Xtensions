28ed6f2c9e21fbf133f62f41279f50bb47eb0a62
SuperDroidBond <superdroidbond@yahoo.com>
2018-09-06 12:19:32 +0530

    Initiate our Xtensions, Based on NOS Custom Settings
    Special Thanks to @xyyx

---
+++ src/com/msm/xtended/fragments/NotificationSettings.java
@@ -0,0 +1,23 @@
+package com.msm.xtended.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import com.android.settings.R;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class NotificationSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.x_settings_notifications);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.XTENSIONS;
+    }
+}
