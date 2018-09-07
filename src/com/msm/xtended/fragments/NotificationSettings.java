12e8991a5a87f5211a7e506a289ae4d4888e02c4
PMS22 <prathams99@rediff.com>
2018-09-07 13:43:04 +0530

    Add Flashlight blink on incoming calls [2/3]
    - Thanks to @shripal17 for suggesting list preference
      instead of a toggle
    - Add a device specific flashlight check

--- src/com/msm/xtended/fragments/NotificationSettings.java
+++ src/com/msm/xtended/fragments/NotificationSettings.java
@@ -2,20 +2,29 @@ package com.msm.xtended.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import com.msm.xtended.preferences.XUtils;
 
+import android.content.Context;
+import android.content.ContentResolver;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.os.Bundle;
 import com.android.settings.R;
 
 import com.android.settings.SettingsPreferenceFragment;
 
-public class NotificationSettings extends SettingsPreferenceFragment {
+public class NotificationSettings extends SettingsPreferenceFragment 
+                         implements OnPreferenceChangeListener {
 
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+    private static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
 
     private Preference mChargingLeds;
+    private ListPreference mFlashlightOnCall;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -35,6 +44,32 @@ public class NotificationSettings extends SettingsPreferenceFragment {
                         com.android.internal.R.bool.config_intrusiveBatteryLed)) {
             prefScreen.removePreference(mChargingLeds);
         }
+
+        mFlashlightOnCall = (ListPreference) findPreference(FLASHLIGHT_ON_CALL);
+        Preference FlashOnCall = findPreference("flashlight_on_call");
+        int flashlightValue = Settings.System.getInt(getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL, 1);
+        mFlashlightOnCall.setValue(String.valueOf(flashlightValue));
+        mFlashlightOnCall.setSummary(mFlashlightOnCall.getEntry());
+        mFlashlightOnCall.setOnPreferenceChangeListener(this);
+
+        if (!XUtils.deviceSupportsFlashLight(getActivity())) {
+            prefScreen.removePreference(FlashOnCall);
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+         ContentResolver resolver = getActivity().getContentResolver();
+
+            if (preference == mFlashlightOnCall) {
+               int flashlightValue = Integer.parseInt(((String) newValue).toString());
+               Settings.System.putInt(resolver,
+                     Settings.System.FLASHLIGHT_ON_CALL, flashlightValue);
+               mFlashlightOnCall.setValue(String.valueOf(flashlightValue));
+               mFlashlightOnCall.setSummary(mFlashlightOnCall.getEntry());
+               return true;
+            }
+        return false;
     }
 
     @Override
