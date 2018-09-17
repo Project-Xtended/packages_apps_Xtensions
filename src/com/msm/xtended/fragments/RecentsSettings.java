776f57b431d2dbd6bee1a6421e989f28f30cb600
ganeshi4u <ganeshvarmai4u@gmail.com>
2018-09-17 21:38:55 +0530

    Inform Users about their Dumb Dev

--- src/com/msm/xtended/fragments/RecentsSettings.java
+++ src/com/msm/xtended/fragments/RecentsSettings.java
@@ -35,6 +35,9 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
+	mFooterPreferenceMixin.createFooterPreference()
+		.setTitle(R.string.dumb_dev);
+	
         addPreferencesFromResource(R.xml.x_settings_recents);
 
         ContentResolver resolver = getActivity().getContentResolver();
