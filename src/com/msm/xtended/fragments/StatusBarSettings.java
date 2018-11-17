a6da771b10d5b30395705ac52a9046cbef765157
garwedgess <garethwilliams21@gmail.com>
2018-11-17 20:43:27 +0530

    Breathing missedcall/sms/voicemail [5/5]
    Forward ported from N to P

--- src/com/msm/xtended/fragments/StatusBarSettings.java
+++ src/com/msm/xtended/fragments/StatusBarSettings.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.res.Resources;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -19,6 +20,7 @@ import android.support.v14.preference.PreferenceFragment;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
+import android.net.ConnectivityManager;
 
 import java.util.Locale;
 import android.text.TextUtils;
@@ -39,6 +41,14 @@ import java.util.Collections;
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String SMS_BREATH = "sms_breath";
+    private static final String MISSED_CALL_BREATH = "missed_call_breath";
+    private static final String VOICEMAIL_BREATH = "voicemail_breath";
+
+    private SwitchPreference mSmsBreath;
+    private SwitchPreference mMissedCallBreath;
+    private SwitchPreference mVoicemailBreath;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -46,12 +56,52 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.x_settings_statusbar);
 
         PreferenceScreen prefSet = getPreferenceScreen();
-        final ContentResolver resolver = getActivity().getContentResolver();
+        ContentResolver resolver = getActivity().getContentResolver();
 
+            // Breathing Notifications
+            mSmsBreath = (SwitchPreference) findPreference(SMS_BREATH);
+            mMissedCallBreath = (SwitchPreference) findPreference(MISSED_CALL_BREATH);
+            mVoicemailBreath = (SwitchPreference) findPreference(VOICEMAIL_BREATH);
+
+            ConnectivityManager cm = (ConnectivityManager)
+                    getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            if (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE)) {
+                mSmsBreath.setChecked(Settings.Global.getInt(resolver,
+                        Settings.Global.KEY_SMS_BREATH, 0) == 1);
+                mSmsBreath.setOnPreferenceChangeListener(this);
+
+                mMissedCallBreath.setChecked(Settings.Global.getInt(resolver,
+                        Settings.Global.KEY_MISSED_CALL_BREATH, 0) == 1);
+                mMissedCallBreath.setOnPreferenceChangeListener(this);
+
+                mVoicemailBreath.setChecked(Settings.System.getInt(resolver,
+                        Settings.System.KEY_VOICEMAIL_BREATH, 0) == 1);
+                mVoicemailBreath.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mSmsBreath);
+                prefSet.removePreference(mMissedCallBreath);
+                prefSet.removePreference(mVoicemailBreath);
+            }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+	    ContentResolver resolver = getActivity().getContentResolver();
+
+            if (preference == mSmsBreath) {
+                boolean value = (Boolean) objValue;
+                Settings.Global.putInt(getContentResolver(), SMS_BREATH, value ? 1 : 0);
+                return true;
+            } else if (preference == mMissedCallBreath) {
+                boolean value = (Boolean) objValue;
+                Settings.Global.putInt(getContentResolver(), MISSED_CALL_BREATH, value ? 1 : 0);
+                return true;
+            } else if (preference == mVoicemailBreath) {
+                boolean value = (Boolean) objValue;
+                Settings.System.putInt(getContentResolver(), VOICEMAIL_BREATH, value ? 1 : 0);
+                return true;
+            }
         return false;
     }
 
