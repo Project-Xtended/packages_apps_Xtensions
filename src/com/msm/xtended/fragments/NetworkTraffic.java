19f96452f2c1cdb42ed2e89a62427b9c2c5862c5
SuperDroidBond <superdroidbond@yahoo.com>
2019-03-03 23:26:27 +0530

    Network Traffic:Add Option for Users to select size of Single Traffic (Up/Down) Font Size [2/2]

--- src/com/msm/xtended/fragments/NetworkTraffic.java
+++ src/com/msm/xtended/fragments/NetworkTraffic.java
@@ -39,9 +39,12 @@ import java.util.Collections;
 public class NetworkTraffic extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String NETWORK_TRAFFIC_FONT_SIZE  = "network_traffic_font_size";
+
     private CustomSeekBarPreference mThreshold;
     private SystemSettingSwitchPreference mNetMonitor;
     private ListPreference mNetTrafficType;
+    private CustomSeekBarPreference mNetTrafficSize;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -53,7 +56,7 @@ public class NetworkTraffic extends SettingsPreferenceFragment implements
         final ContentResolver resolver = getActivity().getContentResolver();
 
         boolean isNetMonitorEnabled = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_STATE, 0, UserHandle.USER_CURRENT) == 1;
+                Settings.System.NETWORK_TRAFFIC_STATE, 1, UserHandle.USER_CURRENT) == 1;
         mNetMonitor = (SystemSettingSwitchPreference) findPreference("network_traffic_state");
         mNetMonitor.setChecked(isNetMonitorEnabled);
         mNetMonitor.setOnPreferenceChangeListener(this);
@@ -64,7 +67,12 @@ public class NetworkTraffic extends SettingsPreferenceFragment implements
         mNetTrafficType.setValue(String.valueOf(value));
         mNetTrafficType.setSummary(mNetTrafficType.getEntry());
         mNetTrafficType.setOnPreferenceChangeListener(this);
-        mNetTrafficType.setEnabled(isNetMonitorEnabled);
+
+        mNetTrafficSize = (CustomSeekBarPreference) findPreference(NETWORK_TRAFFIC_FONT_SIZE);
+        int NetTrafficSize = Settings.System.getInt(resolver,
+                Settings.System.NETWORK_TRAFFIC_FONT_SIZE, 15);
+        mNetTrafficSize.setValue(NetTrafficSize / 1);
+        mNetTrafficSize.setOnPreferenceChangeListener(this);
 
         value = Settings.System.getIntForUser(resolver,
                 Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 1, UserHandle.USER_CURRENT);
@@ -90,6 +98,11 @@ public class NetworkTraffic extends SettingsPreferenceFragment implements
             Settings.System.putIntForUser(getContentResolver(),
                     Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, val,
                     UserHandle.USER_CURRENT);
+            return true;
+        }  else if (preference == mNetTrafficSize) {
+            int width = ((Integer)objValue).intValue();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NETWORK_TRAFFIC_FONT_SIZE, width);
             return true;
         } else if (preference == mNetTrafficType) {
             int val = Integer.valueOf((String) objValue);
