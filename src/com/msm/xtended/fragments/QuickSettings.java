dcb528008feede60566c524b10309a20212ea21c
ezio84 <brabus84@gmail.com>
2019-11-18 23:20:27 +0530

    Allow to set QS panel rows [2/2]

--- src/com/msm/xtended/fragments/QuickSettings.java
+++ src/com/msm/xtended/fragments/QuickSettings.java
@@ -36,10 +36,14 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String PREF_COLUMNS_PORTRAIT = "qs_columns_portrait";
     private static final String PREF_COLUMNS_LANDSCAPE = "qs_columns_landscape";
     private static final String PREF_COLUMNS_QUICKBAR = "qs_columns_quickbar";
+    private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
+    private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
 
     private CustomSeekBarPreference mQsColumnsPortrait;
     private CustomSeekBarPreference mQsColumnsLandscape;
     private CustomSeekBarPreference mQsColumnsQuickbar;
+    private CustomSeekBarPreference mQsRowsPortrait;
+    private CustomSeekBarPreference mQsRowsLandscape;
     private ListPreference mQuickPulldown;
 
     @Override
@@ -75,6 +79,18 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.System.QS_QUICKBAR_COLUMNS, 6);
         mQsColumnsQuickbar.setValue(columnsQuickbar);
         mQsColumnsQuickbar.setOnPreferenceChangeListener(this);
+
+        mQsRowsPortrait = (CustomSeekBarPreference) findPreference(PREF_ROWS_PORTRAIT);
+        int rowsPortrait = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_LAYOUT_ROWS, 3, UserHandle.USER_CURRENT);
+        mQsRowsPortrait.setValue(rowsPortrait);
+        mQsRowsPortrait.setOnPreferenceChangeListener(this);
+
+        mQsRowsLandscape = (CustomSeekBarPreference) findPreference(PREF_ROWS_LANDSCAPE);
+        int rowsLandscape = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 2, UserHandle.USER_CURRENT);
+        mQsRowsLandscape.setValue(rowsLandscape);
+        mQsRowsLandscape.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -100,6 +116,16 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             int value = (Integer) newValue;
             Settings.System.putIntForUser(resolver,
                     Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE, value, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mQsRowsPortrait) {
+            int value = (Integer) newValue;
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.QS_LAYOUT_ROWS, value, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mQsRowsLandscape) {
+            int value = (Integer) newValue;
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, value, UserHandle.USER_CURRENT);
             return true;
         }
         return false;
