ba31bf3b2053b8f92934afefee0ead7adc0ac2e4
Alex <du.alexcruz@gmail.com>
2020-04-02 03:55:49 -0400

    Team Dialog: Redesign
    - Move to a bottom sheet dialog because that's the new shit :p
    - Added animations that we have with screen record
    - Added github links
    - Added titles and github user names to developer sections
    - Moved to a recyclerview style so we're not having to add individual layouts
    - Added Randall and Frank to our ranks
    - Updated pics for everyone on the team per Telegram avatars
    
    Credit/Thanks to Neil Hainsworth for Github svg's
    https://www.iconfinder.com/iconsets/free-social-icons
    
    xawlw:
    Credit/Thanks to GreenTurtwig for Xtension's icon
    https://materialdesignicons.com/icon/account-group
    
    Change-Id: Idea96f461b17bf7759e15d074b53d3085b5036d1

---
+++ src/com/msm/xtended/fragments/TeamActivity.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2020 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.msm.xtended.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.settings.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeamActivity extends Activity {
+
+    private List<DevInfoAdapter> mList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.team_recyclerview);
+
+        initTeam();
+
+        Window window = getWindow();
+        window.setGravity(Gravity.BOTTOM);
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT);
+    }
+    private void initTeam(){
+        RecyclerView mRecycleview = findViewById(R.id.listView);
+
+        setTeamMember("SuperDroidBond", getString(R.string.developer_title), "SuperDroidBond",  R.drawable.bond);
+
+        ListAdapter mAdapter = new ListAdapter(mList);
+        mRecycleview.setAdapter(mAdapter);
+        mRecycleview.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void setTeamMember(String devName, String devTitle,
+                               String githubLink, int devImage) {
+        DevInfoAdapter adapter;
+
+        adapter = new DevInfoAdapter();
+        adapter.setImage(devImage);
+        adapter.setDevName(devName);
+        adapter.setDevTitle(devTitle);
+        adapter.setGithubName(githubLink);
+        mList.add(adapter);
+    }
+}
