f8731d49a3eb170b2e595978df1bb474766489b3
bigrushdog <randall.rushing@gmail.com>
2020-07-19 16:21:01 +0530

    Pulse initial checkin for Android 11 [2/2]
    Xtensions: Pulse navbar audio gfx visualizer for Android 10 [3/4]
    
    Change-Id: Icfc9c098fd4c8f8c4f22175a07b1be00217725e1
    
    Extend NavBar Pulse to LockScreen [3/3]
    
    Change-Id: I3ada2ec4cf224273c9103baf474dcedce43f9258
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

---
+++ src/com/xtended/fragments/PulseSettings.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (C) 2020 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xtended.fragments;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.UserHandle;
+
+import androidx.preference.PreferenceCategory;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.xtended.support.colorpicker.ColorPickerPreference;
+import com.xtended.support.preferences.SecureSettingSeekBarPreference;
+import com.xtended.support.preferences.SecureSettingSwitchPreference;
+import com.xtended.support.preferences.SecureSettingListPreference;
+
+import android.provider.Settings;
+
+@SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
+public class PulseSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = PulseSettings.class.getSimpleName();
+    private static final String PULSE_COLOR_MODE_KEY = "pulse_color_mode";
+    private static final String PULSE_COLOR_MODE_CHOOSER_KEY = "pulse_color_user";
+    private static final String PULSE_COLOR_MODE_LAVA_SPEED_KEY = "pulse_lavalamp_speed";
+    private static final String PULSE_RENDER_CATEGORY_SOLID = "pulse_2";
+    private static final String PULSE_RENDER_CATEGORY_FADING = "pulse_fading_bars_category";
+    private static final String PULSE_RENDER_MODE_KEY = "pulse_render_style";
+    private static final int RENDER_STYLE_FADING_BARS = 0;
+    private static final int RENDER_STYLE_SOLID_LINES = 1;
+    private static final int COLOR_TYPE_ACCENT = 0;
+    private static final int COLOR_TYPE_USER = 1;
+    private static final int COLOR_TYPE_LAVALAMP = 2;
+
+    static final int DEFAULT = 0xffffffff;
+
+    private Preference mRenderMode;
+    private SecureSettingListPreference mColorModePref;
+    private ColorPickerPreference mColorPickerPref;
+    private Preference mLavaSpeedPref;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.x_pulse_settings);
+
+        int intColor;
+        String hexColor;
+
+        mColorModePref = (SecureSettingListPreference) findPreference(PULSE_COLOR_MODE_KEY);
+        mLavaSpeedPref = findPreference(PULSE_COLOR_MODE_LAVA_SPEED_KEY);
+        mColorModePref.setOnPreferenceChangeListener(this);
+
+        mColorPickerPref = (ColorPickerPreference) findPreference(PULSE_COLOR_MODE_CHOOSER_KEY);
+        mColorPickerPref.setOnPreferenceChangeListener(this);
+        intColor = Settings.Secure.getInt(getContentResolver(),
+                   Settings.Secure.PULSE_COLOR_USER, DEFAULT);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mColorPickerPref.setSummary(hexColor);
+        mColorPickerPref.setNewPreviewColor(intColor);
+
+        int colorMode = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.PULSE_COLOR_MODE, COLOR_TYPE_LAVALAMP, UserHandle.USER_CURRENT);
+        updateColorPrefs(colorMode);
+
+        mRenderMode = findPreference(PULSE_RENDER_MODE_KEY);
+        mRenderMode.setOnPreferenceChangeListener(this);
+        int renderMode = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.PULSE_RENDER_STYLE, 0, UserHandle.USER_CURRENT);
+        updateRenderCategories(renderMode);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mColorModePref)) {
+            updateColorPrefs(Integer.valueOf(String.valueOf(newValue)));
+            return true;
+        } else if (preference.equals(mColorPickerPref)) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.Secure.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.Secure.PULSE_COLOR_USER, intHex);
+            return true;
+        } else if (preference.equals(mRenderMode)) {
+            updateRenderCategories(Integer.valueOf(String.valueOf(newValue)));
+            return true;
+        }
+        return false;
+    }
+
+    private void updateColorPrefs(int val) {
+        switch (val) {
+            case COLOR_TYPE_ACCENT:
+                mColorPickerPref.setEnabled(false);
+                mLavaSpeedPref.setEnabled(false);
+                break;
+            case COLOR_TYPE_USER:
+                mColorPickerPref.setEnabled(true);
+                mLavaSpeedPref.setEnabled(false);
+                break;
+            case COLOR_TYPE_LAVALAMP:
+                mColorPickerPref.setEnabled(false);
+                mLavaSpeedPref.setEnabled(true);
+                break;
+        }
+    }
+
+    private void updateRenderCategories(int mode) {
+        PreferenceCategory fadingBarsCat = (PreferenceCategory) findPreference(
+                PULSE_RENDER_CATEGORY_FADING);
+        fadingBarsCat.setEnabled(mode == RENDER_STYLE_FADING_BARS);
+        PreferenceCategory solidBarsCat = (PreferenceCategory) findPreference(
+                PULSE_RENDER_CATEGORY_SOLID);
+        solidBarsCat.setEnabled(mode == RENDER_STYLE_SOLID_LINES);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.XTENDED;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.x_pulse_settings);
+}
+
+
