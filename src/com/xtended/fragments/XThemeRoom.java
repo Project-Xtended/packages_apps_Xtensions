b5df67fb3a4e797c59eeee928d9e0dac6ddeb3f0
Sonal Singh <sonal.singh.19993@gmail.com>
2021-04-26 11:17:21 +0530

    Xtensions: add all themeing related stuffs in ThemeRoom
     - Dark UI Mode
     - Fonts picker
     - Icon shapes
     - Icon packs
     - Clock styles picker
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- src/com/xtended/fragments/XThemeRoom.java
+++ src/com/xtended/fragments/XThemeRoom.java
@@ -36,6 +36,8 @@ import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.os.ServiceManager;
 import android.os.UserHandle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
@@ -48,6 +50,10 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.development.OverlayCategoryPreferenceController;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+
 import com.xtended.support.colorpicker.ColorPickerPreference;
 
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -94,6 +100,24 @@ public class XThemeRoom extends DashboardFragment implements
         mMonetColor.setNewPreviewColor(intColor);
         mMonetColor.setSummary(hexColor);
         mMonetColor.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.font"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.adaptive_icon_shape"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.icon_pack.android"));
+        return controllers;
     }
 
     @Override
