c5567b1464b833ac57c1b7ad1cc22f6b467c2afe
beanstown106 <nbenis106@gmail.com>
2021-04-30 22:28:54 +0530

    Xtensions: Fingerprint authentication vibration [2/2]
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- src/com/xtended/fragments/LockScreenSettings.java
+++ src/com/xtended/fragments/LockScreenSettings.java
@@ -55,8 +55,11 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String FP_KEYSTORE = "fp_unlock_keystore";
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
 
     private SystemSettingSwitchPreference mFingerprintUnlock;
+    private FingerprintManager mFingerprintManager;
+    private SwitchPreference mFingerprintVib;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -77,10 +80,26 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
                mFingerprintUnlock.setSummary(R.string.fp_unlock_keystore_summary);
             }
         }
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintVib = (SwitchPreference) findPreference(FINGERPRINT_VIB);
+        if (!mFingerprintManager.isHardwareDetected()){
+            prefScreen.removePreference(mFingerprintVib);
+        } else {
+            mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
+            mFingerprintVib.setOnPreferenceChangeListener(this);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mFingerprintVib) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, value ? 1 : 0);
+            return true;
+        }
         return false;
     }
 
