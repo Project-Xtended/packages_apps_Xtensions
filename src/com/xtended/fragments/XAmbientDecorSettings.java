d28e6088fac24107efc0231459e868fa2d56b6bf
Pranav Vashi <neobuddy89@gmail.com>
2021-05-15 22:32:30 +0530

    Xtensions: Import X-Ambient Doze Option [2/3]
    SDB: kanged from CrDroid commit -
    
    https://github.com/crdroidandroid/android_packages_apps_crDroidSettings/commit/84c28a2d66a266e31991c2dc618b63d4f4496e60

--- src/com/xtended/fragments/XAmbientDecorSettings.java
+++ src/com/xtended/fragments/XAmbientDecorSettings.java
@@ -44,11 +44,15 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import android.provider.SearchIndexableResource;
 
+import com.xtended.preferences.DozeUtils;
 import com.android.settings.SettingsPreferenceFragment;
 import com.xtended.support.preferences.CustomSeekBarPreference;
 import com.xtended.support.preferences.SystemSettingSeekBarPreference;
 import com.xtended.support.preferences.SystemSettingSwitchPreference;
 import com.xtended.support.preferences.SecureSettingSwitchPreference;
+
+import java.util.List;
+import java.util.ArrayList;
 
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class XAmbientDecorSettings extends SettingsPreferenceFragment
@@ -56,19 +60,134 @@ public class XAmbientDecorSettings extends SettingsPreferenceFragment
 
     public static final String TAG = "XAmbientDecorSettings";
 
+    private static final String KEY_DOZE_ALWAYS_ON = "doze_always_on";
+
+    private static final String CATEG_DOZE_SENSOR = "doze_sensor";
+
+    private static final String KEY_DOZE_TILT_GESTURE = "doze_tilt_gesture";
+    private static final String KEY_DOZE_PICK_UP_GESTURE = "doze_pick_up_gesture";
+    private static final String KEY_DOZE_HANDWAVE_GESTURE = "doze_handwave_gesture";
+    private static final String KEY_DOZE_POCKET_GESTURE = "doze_pocket_gesture";
+    private static final String KEY_DOZE_GESTURE_VIBRATE = "doze_gesture_vibrate";
+
+    private SwitchPreference mDozeAlwaysOnPreference;
+    private SwitchPreference mTiltPreference;
+    private SwitchPreference mPickUpPreference;
+    private SwitchPreference mHandwavePreference;
+    private SwitchPreference mPocketPreference;
+
+    private SharedPreferences mPreferences;
+
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         ContentResolver resolver = getActivity().getContentResolver();
         PreferenceScreen prefScreen = getPreferenceScreen();
         addPreferencesFromResource(R.xml.x_ambient_decor);
 
+        Context context = getContext();
+
+        PreferenceCategory dozeSensorCategory =
+                (PreferenceCategory) getPreferenceScreen().findPreference(CATEG_DOZE_SENSOR);
+
+        mDozeAlwaysOnPreference = (SwitchPreference) findPreference(KEY_DOZE_ALWAYS_ON);
+
+        mTiltPreference = (SwitchPreference) findPreference(KEY_DOZE_TILT_GESTURE);
+        mTiltPreference.setOnPreferenceChangeListener(this);
+
+        mPickUpPreference = (SwitchPreference) findPreference(KEY_DOZE_PICK_UP_GESTURE);
+        mPickUpPreference.setOnPreferenceChangeListener(this);
+
+        mHandwavePreference = (SwitchPreference) findPreference(KEY_DOZE_HANDWAVE_GESTURE);
+        mHandwavePreference.setOnPreferenceChangeListener(this);
+
+        mPocketPreference = (SwitchPreference) findPreference(KEY_DOZE_POCKET_GESTURE);
+        mPocketPreference.setOnPreferenceChangeListener(this);
+
+        // Hide sensor related features if the device doesn't support them
+        if (!DozeUtils.getTiltSensor(context) && !DozeUtils.getPickupSensor(context) && !DozeUtils.getProximitySensor(context)) {
+            getPreferenceScreen().removePreference(dozeSensorCategory);
+        } else {
+            if (!DozeUtils.getTiltSensor(context)) {
+                getPreferenceScreen().removePreference(mTiltPreference);
+            } else if (!DozeUtils.getPickupSensor(context)) {
+                getPreferenceScreen().removePreference(mPickUpPreference);
+            } else if (!DozeUtils.getProximitySensor(context)) {
+                getPreferenceScreen().removePreference(mHandwavePreference);
+                getPreferenceScreen().removePreference(mPocketPreference);
+            }
+        }
+
+        // Hides always on toggle if device doesn't support it (based on config_dozeAlwaysOnDisplayAvailable overlay)
+        boolean mAlwaysOnAvailable = getResources().getBoolean(com.android.internal.R.bool.config_dozeAlwaysOnDisplayAvailable);
+        if (!mAlwaysOnAvailable) {
+            getPreferenceScreen().removePreference(mDozeAlwaysOnPreference);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+         Context context = getContext();
          ContentResolver resolver = getActivity().getContentResolver();
+
+         if (preference == mTiltPreference) {
+             boolean value = (Boolean) newValue;
+             Settings.Secure.putIntForUser(resolver, Settings.Secure.DOZE_TILT_GESTURE,
+                  value ? 1 : 0, UserHandle.USER_CURRENT);
+             DozeUtils.enableService(context);
+             if (newValue != null)
+                 sensorWarning(context);
+             return true;
+         } else if (preference == mPickUpPreference) {
+             boolean value = (Boolean) newValue;
+             Settings.Secure.putIntForUser(resolver, Settings.Secure.DOZE_PICK_UP_GESTURE,
+                  value ? 1 : 0, UserHandle.USER_CURRENT);
+             DozeUtils.enableService(context);
+             if (newValue != null)
+                 sensorWarning(context);
+             return true;
+         } else if (preference == mHandwavePreference) {
+             boolean value = (Boolean) newValue;
+             Settings.Secure.putIntForUser(resolver, Settings.Secure.DOZE_HANDWAVE_GESTURE,
+                  value ? 1 : 0, UserHandle.USER_CURRENT);
+             DozeUtils.enableService(context);
+             if (newValue != null)
+                 sensorWarning(context);
+             return true;
+         } else if (preference == mPocketPreference) {
+             boolean value = (Boolean) newValue;
+             Settings.Secure.putIntForUser(resolver, Settings.Secure.DOZE_POCKET_GESTURE,
+                  value ? 1 : 0, UserHandle.USER_CURRENT);
+             DozeUtils.enableService(context);
+             if (newValue != null)
+                 sensorWarning(context);
+             return true;
+         }
          return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    private void sensorWarning(Context context) {
+        mPreferences = context.getSharedPreferences("dozesettingsfragment", Activity.MODE_PRIVATE);
+        if (mPreferences.getBoolean("sensor_warning_shown", false)) {
+            return;
+        }
+        context.getSharedPreferences("dozesettingsfragment", Activity.MODE_PRIVATE)
+                .edit()
+                .putBoolean("sensor_warning_shown", true)
+                .commit();
+
+        new AlertDialog.Builder(context)
+                .setTitle(getResources().getString(R.string.sensor_warning_title))
+                .setMessage(getResources().getString(R.string.sensor_warning_message))
+                .setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                        }
+                }).show();
     }
 
     @Override
