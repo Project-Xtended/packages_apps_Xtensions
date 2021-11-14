d9983abbf5cc2c46c1310919dae1ad9a2cbc774d
SuperDroidBond <superdroidbond@yahoo.com>
2021-11-14 22:04:54 +0530

    Power Menu Animation: move it to animation settings
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- src/com/xtended/fragments/PowerMenuSettings.java
+++ src/com/xtended/fragments/PowerMenuSettings.java
@@ -47,10 +47,6 @@ import java.util.List;
 public class PowerMenuSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
-
-    private ListPreference mPowerMenuAnimations;
-
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -59,23 +55,11 @@ public class PowerMenuSettings extends SettingsPreferenceFragment implements
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
 
-        mPowerMenuAnimations = (ListPreference) findPreference(POWER_MENU_ANIMATIONS);
-        mPowerMenuAnimations.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.POWER_MENU_ANIMATIONS, 0)));
-        mPowerMenuAnimations.setSummary(mPowerMenuAnimations.getEntry());
-        mPowerMenuAnimations.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mPowerMenuAnimations) {
-            Settings.System.putInt(getContentResolver(), Settings.System.POWER_MENU_ANIMATIONS,
-                    Integer.valueOf((String) newValue));
-            mPowerMenuAnimations.setValue(String.valueOf(newValue));
-            mPowerMenuAnimations.setSummary(mPowerMenuAnimations.getEntry());
-            return true;
-        }
         return false;
     }
 
