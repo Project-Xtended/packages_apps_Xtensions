0ff4f742a53028f8ac6394a5c0803e834048c5e6
SagarMakhar <sagarmakhar@gmail.com>
2021-12-29 16:58:56 +0000

    Bring back icon shapes, icon packs, fonts Customisation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- src/com/xtended/fragments/XThemeRoom.java
+++ src/com/xtended/fragments/XThemeRoom.java
@@ -96,12 +96,6 @@ public class XThemeRoom extends DashboardFragment implements
 
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.font"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.adaptive_icon_shape"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.icon_pack.android"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.signal_icon"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.wifi_icon"));
