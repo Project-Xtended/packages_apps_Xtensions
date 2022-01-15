74d8a6cf5c1421d0dd0e259bda457f52b9b6c97d
spkal01 <kalligeross@gmail.com>
2022-01-16 06:20:43 +0800

    Xtensions: Refactor QsClockPicker [2/2]
    Change-Id: I75422255c56cadc66ea7b880fd662e3812d6db2e

--- src/com/xtended/fragments/QuickSettings.java
+++ src/com/xtended/fragments/QuickSettings.java
@@ -20,14 +20,22 @@ package com.xtended.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import static android.os.UserHandle.USER_CURRENT;
+import static android.os.UserHandle.USER_SYSTEM;
 import android.os.Bundle;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.content.ContentResolver;
+import android.database.ContentObserver;
 import android.content.res.Resources;
+import android.net.Uri;
+import android.content.om.IOverlayManager;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
@@ -44,6 +52,7 @@ import android.text.TextUtils;
 import android.view.View;
 
 import com.xtended.support.preferences.SystemSettingEditTextPreference;
+import com.xtended.support.preferences.SystemSettingListPreference;
 
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
@@ -56,8 +65,14 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String X_FOOTER_TEXT_STRING = "x_footer_text_string";
+    private static final String QS_CLOCK_PICKER = "qs_clock_picker";
 
     private SystemSettingEditTextPreference mFooterString;
+    private SystemSettingListPreference mQsClockPicker;
+
+    private IOverlayManager mOverlayManager;
+    private IOverlayManager mOverlayService;
+    private Handler mHandler;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -79,6 +94,57 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             Settings.System.putString(getActivity().getContentResolver(),
                     Settings.System.X_FOOTER_TEXT_STRING, "Xtended");
         }
+        
+        mOverlayManager = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
+        mOverlayService = IOverlayManager.Stub
+                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
+
+        mQsClockPicker = (SystemSettingListPreference) findPreference(QS_CLOCK_PICKER);
+        boolean isAospClock = Settings.System.getIntForUser(resolver,
+                QS_CLOCK_PICKER, 0, UserHandle.USER_CURRENT) == 4;
+        mQsClockPicker.setOnPreferenceChangeListener(this);
+        mCustomSettingsObserver.observe();
+    }
+
+    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+    private class CustomSettingsObserver extends ContentObserver {
+
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            Context mContext = getContext();
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_CLOCK_PICKER ),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(Settings.System.QS_CLOCK_PICKER ))) {
+                updateQsClock();
+            }
+        }
+    }
+
+    private void updateQsClock() {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        boolean AospClock = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.QS_CLOCK_PICKER , 0, UserHandle.USER_CURRENT) == 4;
+        boolean ColorOsClock = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.QS_CLOCK_PICKER , 0, UserHandle.USER_CURRENT) == 5;
+
+        if (AospClock) {
+            updateQsClockPicker(mOverlayManager, "com.spark.qsclockoverlays.aosp");
+        } else if (ColorOsClock) {
+            updateQsClockPicker(mOverlayManager, "com.spark.qsclockoverlays.coloros");
+        } else {
+            setDefaultClock(mOverlayManager);
+        }
     }
 
     @Override
@@ -95,10 +161,56 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                         Settings.System.X_FOOTER_TEXT_STRING, "Xtended");
             }
             return true;
+        } else if (preference == mQsClockPicker) {
+            int SelectedClock = Integer.valueOf((String) newValue);
+            Settings.System.putInt(resolver, Settings.System.QS_CLOCK_PICKER, SelectedClock);
+            mCustomSettingsObserver.observe();
+            return true;
         }
         return false;
     }
 
+    public static void setDefaultClock(IOverlayManager overlayManager) {
+        for (int i = 0; i < CLOCKS.length; i++) {
+            String clocks = CLOCKS[i];
+            try {
+                overlayManager.setEnabled(clocks, false, USER_SYSTEM);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void updateQsClockPicker(IOverlayManager overlayManager, String overlayName) {
+        try {
+            for (int i = 0; i < CLOCKS.length; i++) {
+                String clocks = CLOCKS[i];
+                try {
+                    overlayManager.setEnabled(clocks, false, USER_SYSTEM);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+            overlayManager.setEnabled(overlayName, true, USER_SYSTEM);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void handleOverlays(String packagename, Boolean state, IOverlayManager mOverlayManager) {
+        try {
+            mOverlayManager.setEnabled(packagename,
+                    state, USER_SYSTEM);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final String[] CLOCKS = {
+        "com.spark.qsclockoverlays.aosp",
+        "com.spark.qsclockoverlays.coloros",
+    };
+    
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.XTENSIONS;
