3b6736d960171f087e186c7af34c9708f483eb4f
SuperDroidBond <superdroidbond@yahoo.com>
2022-01-20 03:09:25 +0530

    Wifi-Signal Icons: make preference similar to others
    SDB: idea n files taken from 2e599e14a29951ae9a8c4ef050703c91e0f2c3a0
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- src/com/xtended/fragments/XThemeRoom.java
+++ src/com/xtended/fragments/XThemeRoom.java
@@ -111,16 +111,6 @@ public class XThemeRoom extends DashboardFragment implements
             Context context, Lifecycle lifecycle, Fragment fragment) {
 
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.font"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.adaptive_icon_shape"));
-        //controllers.add(new OverlayCategoryPreferenceController(context,
-        //        "android.theme.customization.icon_pack.android"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.signal_icon"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.wifi_icon"));
         return controllers;
     }
 
