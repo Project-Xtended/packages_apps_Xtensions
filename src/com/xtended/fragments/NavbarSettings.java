115ae4cff5c5b4c67069f5504d2317f899af3a7b
SuperDroidBond <superdroidbond@yahoo.com>
2022-02-02 14:04:05 +0530

    Xtensions: prepare for new rebased sources
    1) Revert " Xtensions: Allow disabling SmartSpace [2/2]"
    
    This reverts commit 5a1a895
    
    2) Revert "Navbar: Arrow keys while typing resurrection"
    
    This reverts commit 6127e23750cdabe8aa9939e3d3d7c5d891546fdf.
    
    3) Revert "Xtensions: Add tunables for navbar layout customization [2/2]"
    
    This reverts commit a0f957ff7329f7f503525c640fe6db3bb6dede00.
    
    4) Revert "Navbar: Add setting to toggle navbar"
    
    This reverts commit 3e5d060dba3a37af9c7e4c1fa2dbc0a5a904ed66.
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- src/com/xtended/fragments/NavbarSettings.java
+++ src/com/xtended/fragments/NavbarSettings.java
@@ -42,72 +42,22 @@ import android.provider.Settings;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
-import com.android.internal.util.xtended.XtendedUtils;
 import com.android.settings.R;
 
 public class NavbarSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
-
-    private static final String KEY_NAVIGATION_BAR_ENABLED = "force_show_navbar";
-    private static final String LAYOUT_SETTINGS = "navbar_layout_views";
-    private static final String NAVIGATION_BAR_INVERSE = "navbar_inverse_layout";
-
-    private SwitchPreference mNavigationBar;
-    private boolean mIsNavSwitchingMode = false;
-    private Preference mLayoutSettings;
-    private SwitchPreference mSwapNavButtons;
-    private Handler mHandler;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.x_settings_navigation);
 
-        final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
-
-        mHandler = new Handler();
-
-        final boolean defaultToNavigationBar = getResources().getBoolean(
-                com.android.internal.R.bool.config_showNavigationBar);
-        final boolean navigationBarEnabled = Settings.System.getIntForUser(
-                resolver, Settings.System.FORCE_SHOW_NAVBAR,
-                defaultToNavigationBar ? 1 : 0, UserHandle.USER_CURRENT) != 0;
-
-        mNavigationBar = (SwitchPreference) findPreference(KEY_NAVIGATION_BAR_ENABLED);
-        mNavigationBar.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.FORCE_SHOW_NAVBAR,
-                defaultToNavigationBar ? 1 : 0) == 1));
-        mNavigationBar.setOnPreferenceChangeListener(this);
-
-        mLayoutSettings = findPreference(LAYOUT_SETTINGS);
-        mSwapNavButtons = findPreference(NAVIGATION_BAR_INVERSE);
-
-        if (!XtendedUtils.isThemeEnabled("com.android.internal.systemui.navbar.threebutton")) {
-            mLayoutSettings.setEnabled(false);
-            mSwapNavButtons.setEnabled(false);
-        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mNavigationBar) {
-            boolean value = (Boolean) newValue;
-            if (mIsNavSwitchingMode) {
-                return false;
-            }
-            mIsNavSwitchingMode = true;
-            Settings.System.putInt(resolver, Settings.System
-            .FORCE_SHOW_NAVBAR, value ? 1 : 0);
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mIsNavSwitchingMode = false;
-                }
-            }, 1500);
-            return true;
-        }
         return false;
     }
 
